73e5e3548a1bf03040f2009810d9ef27d0d553e4
YangShiJun <1042166341@qq.com>
2020-10-16 09:36:29 +0800

    axios配置设置 响应拦截

--- src/main.js
+++ src/main.js
@@ -1,10 +1,28 @@
 import Vue from 'vue'
-import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import App from './App.vue'
+import { response } from 'express'
+
 
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios
 
+//设置axios的基础配置
+axios.defaults.baseURL = '/api'
+axios.defaults.timeout = 8000
 
+//响应拦截
+axios.interceptors.response.use(function(response) {
+    let res = response.data;
+    if (res.status == 0) {
+        return res.data
+    } else if (res.status == 10) {
+        window.location.href = '/#/login'
+    } else {
+        alert(res.msg)
+    }
+})
 
 new Vue({
     router,
