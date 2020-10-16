39041f2e9d101edd8e4d0ac7d2b6bc9dd87f976e
YangShiJun <1042166341@qq.com>
2020-10-16 10:34:11 +0800

    环境的设置

--- src/main.js
+++ src/main.js
@@ -2,15 +2,18 @@ import Vue from 'vue'
 import router from './router'
 import axios from 'axios'
 import App from './App.vue'
-import { response } from 'express'
+import env from './env'
 
 
 Vue.config.productionTip = false
 Vue.prototype.$axios = axios
 
-//设置axios的基础配置
+//代理方式设置axios的基础配置
 axios.defaults.baseURL = '/api'
 axios.defaults.timeout = 8000
+
+//jsonp cors方式获取基地址
+axios.defaults.baseURL = env.baseURL
 
 //响应拦截
 axios.interceptors.response.use(function(response) {
--- src/storage/index.js
+++ src/storage/index.js
@@ -32,7 +32,9 @@ export default {
     clear(key, module_name) {
         let val = this.getStorage()
         if (module_name) {
-            delete val[module_name][key]
+            if (val[module_name]) {
+                delete val[module_name][key]
+            }
         } else {
             delete val[key]
         }
